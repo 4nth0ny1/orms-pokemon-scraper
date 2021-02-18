7e5b6cd9a40ad14f22d8630d794af204e1047fca
4anth0ny1 <acatullo4@gmail.com>
2021-02-17 19:23:01 -0600

    commit tree f55b3c4e55418bc82c54f761956c386971b66914

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -12,30 +12,10 @@ class Pokemon
     def self.save(name, type, db)
         db.execute("INSERT INTO pokemon (name, type) VALUES (?, ?);", name, type)
     end
-        # sql = <<-SQL
-        #   INSERT INTO pokemon (name, type, db) 
-        #   VALUES (?, ?)
-        # SQL
-    
-        # DB[:conn].execute(sql, self.name, self.type, self.db)
-    
-        # @id = DB[:conn].execute("SELECT last_insert_rowid() FROM pokemon")[0][0]
-    
 
     def self.find(id, db)
         poke = db.execute("SELECT * FROM pokemon WHERE id = ?;", id).flatten
         x = Pokemon.new(id: poke[0], name: poke[1], type: poke[2], db: db)
     end
-        # sql = <<-SQL
-        #   SELECT *
-        #   FROM pokemon
-        #   WHERE id = ?
-        #   LIMIT 1
-        # SQL
-        
-        # DB[:conn].execute(sql, name).map do |row|
-        #   self.new_from_db(row)
-        # end.first
-    
 
 end
